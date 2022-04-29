14b258a87a8e704882b6a704bc7411813e0102f7
ethanchen7 <ethan.chen7@gmail.com>
2022-04-29 10:22:38 -0700

    production scripts

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,7 @@
 const express = require("express");
-
+const router = express.Router();
 const apiRouter = require("./api");
 
-const router = express.Router();
 router.use("/api", apiRouter);
 
 // Static routes
@@ -12,7 +11,7 @@ if (process.env.NODE_ENV === "production") {
   // Serve the frontend's index.html file at the root route
   router.get("/", (req, res) => {
     res.cookie("XSRF-TOKEN", req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, "../../frontend", "build", "index.html")
     );
   });
@@ -23,7 +22,7 @@ if (process.env.NODE_ENV === "production") {
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie("XSRF-TOKEN", req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, "../../frontend", "build", "index.html")
     );
   });
@@ -33,7 +32,7 @@ if (process.env.NODE_ENV === "production") {
 if (process.env.NODE_ENV !== "production") {
   router.get("/api/csrf/restore", (req, res) => {
     res.cookie("XSRF-TOKEN", req.csrfToken());
-    return res.json({});
+    res.status(201).json({});
   });
 }
 
