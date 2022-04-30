9e76a189e44a08abd382f4f0cd304f2a1c71a6b6
ethanchen7 <ethan.chen7@gmail.com>
2022-04-29 20:38:19 -0700

    splash page animations

--- frontend/src/components/SplashBody/index.js
+++ frontend/src/components/SplashBody/index.js
@@ -1,8 +1,25 @@
+import "./SplashBody.css";
 const SplashBody = () => {
   return (
-    <div>
-      <h1>Find your inspiration</h1>
-    </div>
+    <>
+      <div className="splash-body-container">
+        <h1>Find your inspiration.</h1>
+        <p>
+          Join the Travelr community, home to travel enthusiasts and explorists.
+        </p>
+      </div>
+      <ul className="slideshow">
+        <li>
+          <span>Image 01</span>
+        </li>
+        <li>
+          <span>Image 02</span>
+        </li>
+        <li>
+          <span>Image 03</span>
+        </li>
+      </ul>
+    </>
   );
 };
 
