e8c69e900a93c9b8e97439ecf9ef23d6f4c79688
ethanchen7 <ethan.chen7@gmail.com>
2022-05-01 14:52:36 -0700

    userPage css

---
+++ frontend/src/components/UserPage/index.js
@@ -0,0 +1,12 @@
+import UserPageHeader from "../UserPageHeader";
+import "./UserPage.css";
+
+const UserPage = () => {
+  return (
+    <div className="user-page-container">
+      <UserPageHeader />
+    </div>
+  );
+};
+
+export default UserPage;
