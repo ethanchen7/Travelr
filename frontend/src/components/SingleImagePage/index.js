1891e6a4a6b23f62b2d4f719c2cddfc90eb1d33d
ethanchen7 <ethan.chen7@gmail.com>
2022-05-03 19:51:43 -0700

    dynamically rendering comments and usernames

--- frontend/src/components/SingleImagePage/index.js
+++ frontend/src/components/SingleImagePage/index.js
@@ -2,18 +2,23 @@ import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getSingleImage } from "../../store/image";
+import { getComments } from "../../store/comment";
 import "./SingleImagePage.css";
 
 const SingleImagePage = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
   const imageId = parseInt(id);
+  const sessionUser = useSelector((state) => state.session.user);
   const image = useSelector((state) => state.images.imageObjects)[imageId];
+  const commentObjects = useSelector((state) => state.comment.imageComments);
+  const comments = Object.values(commentObjects);
 
   useEffect(() => {
     dispatch(getSingleImage(imageId));
+    dispatch(getComments(imageId));
   }, [dispatch]);
-
+  console.log(comments);
   return (
     <div className="single-image-body-container">
       <div className="image-container">
@@ -37,7 +42,12 @@ const SingleImagePage = () => {
               <div className="line-break"></div>
             </div>
             <div className="comment-container">
-              <div className="comment"></div>
+              {comments?.map((comment) => (
+                <div className="comment" key={`${comment.id}`}>
+                  <p>{`@${comment.User.username}`}</p>
+                  <p>{comment.text}</p>
+                </div>
+              ))}
             </div>
           </div>
         </div>
