c4576b42599842047c8a9f7189ef381cf3f85147
ethanchen7 <ethan.chen7@gmail.com>
2022-05-06 16:48:49 -0700

    added null file handling in put profile route

--- backend/routes/api/profile.js
+++ backend/routes/api/profile.js
@@ -58,7 +58,7 @@ router.put(
     } = req.body;
 
     let editedPic;
-    if (!profilePic) {
+    if (!profilePic && req.file) {
       editedPic = await singlePublicFileUpload(req.file);
     } else {
       editedPic = profilePic;
